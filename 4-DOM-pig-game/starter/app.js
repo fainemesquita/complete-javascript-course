9449e08133a20452270ffac54646ff27a8c083bc
faine <faine.mesquita@gmail.com>
2019-04-08 14:21:37 +1000

    updates

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -15,12 +15,39 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; same result but with HTML injection instead of plain text
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    //1. random number
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. update the score if dice !=1
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; same result but with HTML injection instead of plain text
+
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
