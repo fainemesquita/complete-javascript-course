fe3caac90576c870ee8dd89b0040f946f322a0ec
faine <faine.mesquita@gmail.com>
2019-04-15 16:50:19 +1000

    ping game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,33 +9,104 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, dice, gamePlaying;
+init();
 
-scores = [0,0];
+//operations with .btn-roll
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    if(gamePlaying) {
+        //1. random number
+        dice = Math.floor(Math.random() * 6) + 1;
+
+        //2. display result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        //3. update the score if dice !=1
+        if (dice !== 1) {
+            //add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        }else {
+            //next player
+            nextPlayer();
+
+        }
+
+    }
+    
+});
+
+
+//operations with .btn-hold
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    if (gamePlaying){
+        //1. add current score to global score
+        scores[activePlayer] += roundScore;
+
+        //2. Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        //3. Check if player won the game
+        if ( scores[activePlayer] >= 20){
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            //4. Next player
+            nextPlayer();
+        }
+    }
+});
+
+
+function nextPlayer(){
+//next player
+activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 roundScore = 0;
-activePlayer = 1;
 
+document.querySelector('#current-0').textContent = '0';
+document.querySelector('#current-1').textContent = '0';
+
+// add class if inexistent or remove if already there
+document.querySelector('.player-0-panel').classList.toggle('active');
+document.querySelector('.player-1-panel').classList.toggle('active');
+
+// alternate version
+// document.querySelector('.player-0-panel').classList.remove('active');
+// document.querySelector('.player-1-panel').classList.add('active');
+
+//hide dice again
 document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-document.querySelector('.btn-roll').addEventListener('click', function(){
-    //1. random number
-    dice = Math.floor(Math.random() * 6) + 1;
-
-    //2. display result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
-
-    //3. update the score if dice !=1
-})
+}
 
 
+document.querySelector('.btn-new').addEventListener('click', init);
 
+function init(){
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    //remove active class from both and add to player 1. It's necessary to remove otherwise the element has two "active" classes
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
 
 
 
