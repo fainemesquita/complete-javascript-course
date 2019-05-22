03a1bc139679fea8383130ce4c241b3b971af2c4
faine <faine.mesquita@gmail.com>
2019-05-22 13:55:48 +1000

    Pig game challenge 3

--- 4-DOM-pig-game/starter/challenge.js
+++ 4-DOM-pig-game/starter/challenge.js
@@ -14,41 +14,34 @@ Change the game to follow these rules:
 var scores, roundScore, activePlayer, dice, dice1, dice2, gamePlaying;
 init();
 
-var previousDice;
+
 
 
 //operations with .btn-roll
 document.querySelector('.btn-roll').addEventListener('click', function(){
     if(gamePlaying) {
         // random number
-        dice = Math.floor(Math.random() * 6) + 1;
+        dice1 = Math.floor(Math.random() * 6) + 1;
         dice2 = Math.floor(Math.random() * 6) + 1;
 
         // display result
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
 
-        var diceDOM = document.querySelector('.dice2');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice2 + '.png';
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
-        if (dice === 6 && previousDice === 6 || dice2 === 6 && previousDice2) {
-            scores[activePlayer] = 0;
-            document.querySelector('#score-' + activePlayer).textContent = 0;
-            nextPlayer();
-
-        }else if (dice !== 1 && dice2 !==1) {
+        if (dice1 !== 1 && dice2 !==1) {
             //add score
-            roundScore += dice + dice2;
+            roundScore += dice1 + dice2;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         }else {
             //next player
             nextPlayer();
             document.querySelector('#current-' + activePlayer).textContent = 0;
         }
-        previousDice = dice;
-        previousDice2 = dice2;
+
+            
     }
     
 });
@@ -78,8 +71,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         //3. Check if player won the game
         if ( scores[activePlayer] >= winningScore){
             document.querySelector('#name-' + activePlayer).textContent = 'Winner';
-            document.querySelector('.dice').style.display = 'none';
-            document.querySelector('.dice2').style.display = 'none';
+            document.getElementByIddocument.getElementById('dice-1').style.display = 'none';
+            document.getElementByIddocument.getElementById('dice-2').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
@@ -108,8 +101,8 @@ document.querySelector('.player-1-panel').classList.toggle('active');
 // document.querySelector('.player-1-panel').classList.add('active');
 
 //hide dice again
-document.querySelector('.dice').style.display = 'none';
-document.querySelector('.dice2').style.display = 'none';
+document.getElementById('dice-1').style.display = 'none';
+document.getElementById('dice-2').style.display = 'none';
 
 }
 
@@ -121,8 +114,8 @@ function init(){
     activePlayer = 0;
     roundScore = 0;
     gamePlaying = true;
-    document.querySelector('.dice').style.display = 'none';
-    document.querySelector('.dice2').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
