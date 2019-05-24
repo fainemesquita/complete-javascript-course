720ab481b3e5ff16e89c39795f098d4c21f91403
faine <faine.mesquita@gmail.com>
2019-05-24 17:02:46 +1000

    closures

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -216,4 +216,22 @@ retirementUS(1990);
 retirementIceland(1990);
 
 //retirement(66)(1990);
+*/
 
+
+
+//job can be accessed even after interviewQuestion has alredy returned because the scope chain is always intact
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+                console.log('What subject do you teach, ' + name + '?');
+        } else {
+                console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+
+interviewQuestion('teacher')('Mark');
