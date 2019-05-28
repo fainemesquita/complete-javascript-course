bfeb8cb093a2c13681d42a934d997d15580ba05c
faine.mesquita <faine.mesquita@gmail.com>
2019-05-28 23:12:12 +1000

    chap6 controllers

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -37,3 +37,75 @@ var controller = (function(budgetCtrl, UICtrl) {
 })(budgetController, UIController);
 
 */
+
+//Budget controller
+var budgetController = (function () {
+    //code
+
+})();
+
+
+//UI controller
+
+var UIController = (function () {
+    
+    //object to store all strings
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+
+    };
+
+    return {
+        //another closure: the object below will have acces to the function bove.
+
+        //method to return all three inputs (object so we can return more than one value)
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        //method exposes DOMstrings to public
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    }
+})();
+
+
+//Global App controller
+
+var controller = (function (budgetCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMstrings();
+    
+    var ctrlAddItem = function () {
+        //event handler to add button
+        // 1. get field input data
+
+        var input = UICtrl.getInput();
+        console.log(input);
+
+        // 2. add item to budget controller
+
+        // 3. add item to UI
+
+        // 4. calculate budget
+
+        // 5. display budget
+
+    };
+    
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    //Anonymous function can also receive parameters.  "event" already tells the browser to listen to any key.
+    document.addEventListener('keypress', function (event) {
+        if (event.keycode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
+
+})(budgetController, UIController);
