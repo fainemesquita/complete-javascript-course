d4c1d3c04c6eda02f8c070fbcb4ace8b3d435cdf
faine.mesquita <faine.mesquita@gmail.com>
2019-07-03 13:34:29 +1000

    not loading recipe

--- 9-forkify/starter/src/js/models/Recipe.js
+++ 9-forkify/starter/src/js/models/Recipe.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { key, proxy } from '../config';
 
+
 export default class Recipe {
   constructor(id) {
     this.id = id;
   }
-
   async getRecipe() {
     try {
       const res = await axios(`${proxy}http://food2fork.com/api/get?key=${key}&rId=${this.id}`);
@@ -14,47 +14,46 @@ export default class Recipe {
       this.img = res.data.recipe.image_url;
       this.url = res.data.recipe.source_url;
       this.ingredients = res.data.recipe.ingredients;
-    } catch (error) {
+      console.log(res);
+    } catch(error){
       console.log(error);
-      alert('Something went wrong :(');
+      alert('something went wrong :(');
     }
   }
 
   calcTime() {
-    // Assuming that we need 15 min for each 3 ingredients
+    //for every 3 ingredients, add 1 period of 15 min
     const numIng = this.ingredients.length;
     const periods = Math.ceil(numIng / 3);
     this.time = periods * 15;
   }
 
-  calcServings() {
+  calcServings(){
     this.servings = 4;
   }
 
   parseIngredients() {
     const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
     const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
-    const units = [...unitsShort, 'kg', 'g'];
 
     const newIngredients = this.ingredients.map(el => {
-      // 1) Uniform units
+      // 1 uniform arrays ( units as they appear vs as we want to display)
       let ingredient = el.toLowerCase();
       unitsLong.forEach((unit, i) => {
         ingredient = ingredient.replace(unit, unitsShort[i]);
       });
-
-      // 2) Remove parentheses
+      // 2 remove parentheses (RegEx)
       ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
 
-      // 3) Parse ingredients into count, unit and ingredient
+      // 3  parse ingredients into count, unit and ingredient
       const arrIng = ingredient.split(' ');
-      const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
+      const unitIndex = arrIng.findIndex(el2 => units.includes(el2)); //check if the element is part of the elements array
 
       let objIng;
       if (unitIndex > -1) {
-        // There is a unit
-        // Ex. 4 1/2 cups, arrCount is [4, 1/2] --> eval("4+1/2") --> 4.5
-        // Ex. 4 cups, arrCount is [4]
+        //there's a unit from the array
+        // ex 4 1/2 cups is [4, 1/2] --> eval(4+1/2)
+        // ex 4 cups is [4]
         const arrCount = arrIng.slice(0, unitIndex);
 
         let count;
@@ -71,35 +70,24 @@ export default class Recipe {
         };
 
       } else if (parseInt(arrIng[0], 10)) {
-        // There is NO unit, but 1st element is number
+        // there's no unit from the array but the first element is a number (1 bread)
         objIng = {
           count: parseInt(arrIng[0], 10),
           unit: '',
-          ingredient: arrIng.slice(1).join(' ')
+          ingredient: arrIng.slice(1).join(' ') //ingredient is the string resultant of the array minus the first element (quantity)
         }
       } else if (unitIndex === -1) {
-        // There is NO unit and NO number in 1st position
+        //there's no number and no unit
         objIng = {
           count: 1,
           unit: '',
           ingredient
         }
+
       }
 
       return objIng;
     });
     this.ingredients = newIngredients;
-  }
-
-  updateServings(type) {
-    // Servings
-    const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
-
-    // Ingredients
-    this.ingredients.forEach(ing => {
-      ing.count *= (newServings / this.servings);
-    });
-
-    this.servings = newServings;
   }
 }
