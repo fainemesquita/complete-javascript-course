b2f199b372e2dbc6359565077d378adb3cf81fd7
faine <faine.mesquita@gmail.com>
2019-07-09 15:28:22 +1000

    list

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/SearchView';
 import * as recipeView from './views/RecipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -107,3 +108,22 @@ elements.searchResPages.addEventListener('click', e => {
 // window.addEventListener('hashchange', controlRecipe);
 // window.addEventListener('load', controlRecipe);
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+      // Decrease button is clicked
+      if (state.recipe.servings > 1) {
+          state.recipe.updateServings('dec');
+          recipeView.updateServingsIngredients(state.recipe);
+      }
+  } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+      // Increase button is clicked
+      state.recipe.updateServings('inc');
+      recipeView.updateServingsIngredients(state.recipe);
+  } 
+  console.log(state.recipe);
+
+});
+
+window.l = new List();
--- 9-forkify/starter/src/js/views/RecipeView.js
+++ 9-forkify/starter/src/js/views/RecipeView.js
@@ -60,7 +60,7 @@ export const renderRecipe = recipe => {
               <svg class="recipe__info-icon">
                   <use href="img/icons.svg#icon-man"></use>
               </svg>
-              <span class="recipe__info-data recipe__info-data--people">{recipe.servings}</span>
+              <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
               <span class="recipe__info-text"> servings</span>
 
               <div class="recipe__info-buttons">
@@ -113,4 +113,16 @@ export const renderRecipe = recipe => {
       </div>
   `;
   elements.recipe.insertAdjacentHTML('afterbegin', markup);
+};
+
+
+export const updateServingsIngredients = recipe => {
+    // Update servings
+    document.querySelector('.recipe__info-data--people').textContent = recipe.servings;
+
+    // Update ingredeints
+    const countElements = Array.from(document.querySelectorAll('.recipe__count'));
+    countElements.forEach((el, i) => {
+        el.textContent = formatCount(recipe.ingredients[i].count);
+    });
 };
